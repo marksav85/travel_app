35286ddabc9e11845446f6c088b034756bcea24e
marksav85 <mark.saville@outlook.com>
2021-04-11 20:22:32 +0200

    Create part of webpack setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,32 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const fetch = require('node-fetch')
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
-// Require Express to run server and routes
-const express = require('express');
 
 // Start up an instance of app
 const app = express();
 
-/* Middleware*/
-const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('dist'));
 
 // // Spin up the server
-const port = 8000
-const server = app.listen(port, listening);
-function listening() {
-  console.log('running on localhost: ' + port);
-  console.log('server running successfully!')
-};
+app.listen(8090, function () {
+    console.log('App successfully listening on port 8090!')
+})
 
 // Initialize all route with a callback function
 app.get('/all', sendData);
