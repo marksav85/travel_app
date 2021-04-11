55407bf747bd1511a66d87228cb41fb6195ca2d3
marksav85 <mark.saville@outlook.com>
2021-04-11 20:54:54 +0200

    Reconfigure webpack functionality

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 const fetch = require('node-fetch')
