aadb170ee8c000bc430fa41a6239377ebae0c92a
marksav85 <mark.saville@outlook.com>
2021-06-18 12:06:29 +0200

    fix: minor changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,8 +6,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     output: {
-        path: path.join(__dirname, "dist"),
-        filename: "main.js",
         libraryTarget: 'var',
         library: 'Client'
     },
