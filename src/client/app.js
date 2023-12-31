3095beaee839c626b4f2685ec612a5542083f466
Mark Saville <mark.saville@outlook.com>
2023-12-31 13:36:51 +0100

    update weather dot notation for api call

--- src/client/app.js
+++ src/client/app.js
@@ -1,17 +1,20 @@
 document
-  .getElementById("weatherForm")
+  .getElementById("destination-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
     const city = document.getElementById("city").value;
-    fetch(`/weather?city=${city}`)
+    fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=52&lon=13&appid="
+    )
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         const weatherDetails = document.getElementById("weather-details");
         weatherDetails.innerHTML = `
-          <h2>Weather in ${data.name}, ${data.sys.country}</h2>
-          <p>Temperature: ${data.main.temp} °C</p>
-          <p>Weather: ${data.weather[0].description}</p>
+          <h2>Weather in ${data.timezone}</h2>
+          <p>Temperature: ${data.current.temp} °C</p>
+          <p>Weather: ${data.current.weather[0].description}</p>
         `;
       })
       .catch((error) => console.error("Error fetching weather data:", error));
