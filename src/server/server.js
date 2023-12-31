f1f7a457e9ac8e816ed00b6cfd95457140ed7c00
Mark Saville <mark.saville@outlook.com>
2023-12-31 13:35:38 +0100

    add console logging for testing

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,11 @@ const fetch = require("node-fetch");
 const app = express();
 // Import API key as environment variable
 require("dotenv").config();
-const apiKey = process.env.API_KEY;
+const myApiKey = process.env.API_KEY;
+console.log(myApiKey);
+
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static("client"));
@@ -17,14 +21,17 @@ app.use(express.static("client"));
 // GET route
 app.get("weather", async (req, res) => {
   try {
-    const apiKey = "";
+    const apiKey = myApiKey;
+    console.log(apiKey);
     //const city = req.query.city || "Berlin"; //Berlin is default city
     const apiUrl =
       req.query.apiUrl ||
       `https://api.openweathermap.org/data/3.0/onecall?lat={52}&lon={13}&appid=${apiKey}`;
     const response = await fetch(apiUrl);
+    console.log(response);
     const data = await response.json();
     res.json(data);
+    console.log(data);
   } catch (err) {
     console.log("Error fetching weather data:", err);
   }
