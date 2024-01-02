0a9e62f70c4cdf6805ff9b0da05b931cff4bd93e
Mark Saville <mark.saville@outlook.com>
2024-01-02 19:45:36 +0100

    tweak styles displaying weather info

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,6 @@
 // add event listener to submit button
+import "./styles/main.css";
+
 document
   .getElementById("destination-form")
   .addEventListener("submit", function (event) {
@@ -14,9 +16,9 @@ document
         const temperatureCelsius = data.main.temp - 273.15;
         // toFixed rounds temperature down to 0 decimal places
         weatherDetails.innerHTML = `
-          <h2>Weather in ${data.name}</h2>
-          <p>Temperature: ${temperatureCelsius.toFixed(0)} °C</p> 
-          <p>Weather: ${data.weather[0].description}</p>
+          <h3>${data.name}</h3>
+          <h4>Temperature: ${temperatureCelsius.toFixed(0)} °C</h4> 
+          <h4>Weather: ${data.weather[0].description}</h4>
         `;
       })
       .catch((error) => console.error("Error fetching weather data:", error));
