4a92c276999487b4bf37d349945e00d70f954661
Mark Saville <mark.saville@outlook.com>
2024-01-02 18:17:01 +0100

    update api call variables

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,6 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Require Express to run server and routes
 const express = require("express");
-//
+// Require Fetch
 const fetch = require("node-fetch");
 // Start up an instance of app
 const app = express();
@@ -15,41 +12,29 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require("cors");
 app.use(cors());
-
-// Import API key as environment variable
+// Require dotenv for environment variable
 require("dotenv").config();
-const myApiKey = process.env.API_KEY;
-console.log(myApiKey);
-
-const cors = require("cors");
-app.use(cors());
-
 // Initialize the main project folder
 app.use(express.static("client"));
-
 // Setup Server
 const port = 8000;
-const server = app.listen(port, listening);
-function listening() {
-  console.log("running on localhost: " + port);
-  console.log("server running successfully!");
-}
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
+// Store environment variable API key in variable
+const apiKey = process.env.API_KEY;
 
 // GET route
-app.get("weather", async (req, res) => {
+app.get("/weather", async (req, res) => {
   try {
-    const apiKey = myApiKey;
-    console.log(apiKey);
-    //const city = req.query.city || "Berlin"; //Berlin is default city
-    const apiUrl =
-      req.query.apiUrl ||
-      `https://api.openweathermap.org/data/3.0/onecall?lat={52}&lon={13}&appid=${apiKey}`;
+    const city = req.query.city || "Berlin"; // Default city is Berlin
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const response = await fetch(apiUrl);
-    console.log(response);
     const data = await response.json();
     res.json(data);
     console.log(data);
   } catch (err) {
     console.log("Error fetching weather data:", err);
+    res.status(500).send("Internal Server Error");
   }
 });
