7adffdff7c47708004da2b736d4e190f8b1a69c1
Mark Saville <mark.saville@outlook.com>
2024-01-03 13:57:51 +0100

    update fetch api to display relevant weather icon

--- src/client/app.js
+++ src/client/app.js
@@ -14,6 +14,8 @@ document
       .then((data) => {
         console.log(data);
         const weatherDetails = document.getElementById("weather-details");
+        const weatherIcon = document.getElementById("weather-icon");
+        const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
         // Convert temperature from Kelvin to Celsius
         const temperatureCelsius = data.main.temp - 273.15;
         // toFixed rounds temperature down to 0 decimal places
@@ -21,6 +23,10 @@ document
           <h3>${data.name}</h3>
           <h4>Temperature: ${temperatureCelsius.toFixed(0)} °C</h4> 
           <h4>Weather: ${data.weather[0].description}</h4>
+          <img src="${iconUrl}" alt="Weather Icon">
+        `;
+        weatherIcon.innerHTML = `
+          <img src="${iconUrl}" alt="Weather Icon">
         `;
       })
       .catch((error) => console.error("Error fetching weather data:", error));
