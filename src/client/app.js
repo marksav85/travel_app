865d82571713e73c0976d525d24dfef90ed09317
Mark Saville <mark.saville@outlook.com>
2024-01-04 18:39:05 +0100

    update api data for rainfall, cloud

--- src/client/app.js
+++ src/client/app.js
@@ -5,7 +5,7 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
     // Get the city name from the input field
-    const city = document.getElementById("city").value;
+    const city = document.getElementById("city").value || "Berlin";
     // Determine the base URL dynamically
     const baseUrl = "http://localhost:8000";
     fetch(`${baseUrl}/weather?city=${city}`)
@@ -18,17 +18,18 @@ document
         const weatherTemperature = document.getElementById(
           "weather-temperature"
         );
-        const weatherType = document.getElementById("weather-type");
-        const weatherIcon = document.getElementById("weather-icon");
+        const weatherClouds = document.getElementById("weather-clouds");
+        const weatherRainfall = document.getElementById("weather-rainfall");
         /* const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`; */
         // Update UI with weather data
-        weatherCity.innerHTML = `<h3>${data.name}</h3>`;
+        weatherCity.innerHTML = `<h3>${city}</h3>`;
         // Convert temperature from Kelvin to Celsius (toFixed rounds temperature down to 0 decimal places)
-        const temperatureCelsius = data.main.temp - 273.15;
-        weatherTemperature.innerHTML = `<h4>Temperature: ${temperatureCelsius.toFixed(
+        const temperatureCelsius = data.temperature.afternoon - 273.15;
+        weatherTemperature.innerHTML = `<h4>Daytime temperature: ${temperatureCelsius.toFixed(
           0
         )} °C</h4>`;
-        weatherType.innerHTML = `<h4>Weather: ${data.weather[0].description}</h4>`;
+        weatherClouds.innerHTML = `<h4>Cloud cover: ${data.cloud_cover.afternoon}</h4>`;
+        weatherRainfall.innerHTML = `<h4>Total rainfall: ${data.precipitation.total}</h4>`;
         /* weatherIcon.innerHTML = `<img src="${iconUrl}" alt="Weather Icon">`; */
       })
       .catch((error) => console.error("Error fetching weather data:", error));
