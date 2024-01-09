e57cd878ef81d7d10058a04bdd243f5e67bee6cc
Mark Saville <mark.saville@outlook.com>
2024-01-09 18:27:24 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/client/views/index.html",
-      filename: "./index.html",
+      filename: "index.html",
+      hash: true,
     }),
     new CleanWebpackPlugin(),
   ],
